98185fb2e2d0a2f4881aff264c7fa779c4ccf6bb
5kyc0d3r <skycoder.official@protonmail.com>
2018-03-09 22:09:09 -0700

    Added function to print program banner

---
+++ rives/core/utils.py
@@ -0,0 +1,10 @@
+"""
+This file is part of the "rives" project.
+Please see the repository's LICENSE file for copying.
+"""
+
+from .variables import RIVES_BANNER
+
+
+def show_banner():
+    print(RIVES_BANNER)
