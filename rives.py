c3ea5eea048c0ad61b710bf0c3e32a6493a4d59e
5kyc0d3r <skycoder.official@protonmail.com>
2018-03-09 22:10:52 -0700

    Added input prompt for command input and use function to print banner

--- rives.py
+++ rives.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 
-from rives.core.variables import RIVES_BANNER
+import rives.core.text_styles as ts
+from rives.core.utils import show_banner
+
+
+def command_input(prompt):
+    return input(ts.white + ts.underline + 'ref' + ts.white + '(' + ts.bold + ts.red + str(prompt) + ts.white + ')>')
 
 
 def main():
-    print(RIVES_BANNER)
+    show_banner()
+
+    while True:
+        command = command_input('console')
 
 
 if __name__ == '__main__':
